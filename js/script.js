69e13d296b6c99c8816728fdb117de311aa3b890
Tasia <tatianka2585@gmail.com>
2025-05-11 12:10:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,141 @@
+"use strict";
+
+// 14. Операторы в JS
+
+// console.log('arr' + "- object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
+// console.log(2*4 == '8');
+// console.log(2*4 === 8);
+
+// &&
+// ||
+
+// const isChecked = true,
+//       isClose = true;
+// console.log(isChecked && isClose);
+
+
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked || isClose);
+
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || isClose);
+
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+// 13. Интерполяция (ES6)
+// const category = 'toys';
+// console.log(`https://someurl.com/'${category}/5`);
+
+// const user = "Tasia";
+// alert (`Hello, ${user}`);
+
+// 12. Простое общение с пользователем
+// alert('Hello');
+// const result = confirm('Are you here?');
+// console.log(result); 
+//  const answer = prompt("Вам є 18?", "18");
+//  console.log(answer+5);
+
+// const answers = [];
+// // answers[0] = prompt ('What is your name?', '');
+// // answers[1] = prompt ('What is your surname?', '');
+// // answers[2] = prompt ('How old are you?', '');
+
+// console.log(typeof(null));
+
+// // // 10.Klasyfikacja typiv da nnyh v JS
+
+// // // let number= 4.6;
+
+// // // console.log(-4/0);
+
+// // // console.log('string' * 9);
+
+// // // const persone = 'Alex';
+
+// // const bool = false;
+
+// // console.log(something);
+
+// // let und;
+// // console.log(und);
+
+// // const obj = {
+// //      name: "John",
+// //      age: 25,
+// //      isMarried: false
+// // };
+// // console.log(obj.name);
+
+// // let arr = ['plum.png', 'orange.jpg', 'apple.bmp', {}, []]
+// // console.log(arr[1]);
+
+
+// // 11. (д) Разница между объектами и массивами и неочевидные синтаксические возможности
+// const arr = ['a', 'b', 'c'];
+
+// arr [10] = '3456';
+// console.log(arr[10]);
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+        
+//     }
+
+// }
+// const b = 'b';
+// // arrObj.b = '1234';
+// arrObj[b] = '1234';
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+
+
+
+// const obj = {
+//     Anna: 500,
+//     'Alice' = 800
+// }
+// lesson 6
+// // let number = 5;
+// // const leftBorderWidth = 1;
+
+// // number = 10;
+// // console.log(number);
+
+// // const obj = {
+// //     a: 50
+// // };
+
+// // obj.a = 10;
+
+// // console. log (obj);
+  
+
